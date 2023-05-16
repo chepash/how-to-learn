00fc1656cd94221ab153514295e09910c1e811f7
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-05-17 01:04:41 +0400

    feat: add config for gh-pages deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/how-to-learn/" : "/",
   transpileDependencies: true,
-});
+};
