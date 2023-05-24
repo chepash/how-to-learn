8f1f17d318b3d7d178ad5e23c902df260e1f3a4c
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-05-24 20:04:11 +0400

    feat: add placeHolder for mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
-  <MainPage />
+  <MainPage v-if="windowWidth >= 1100" />
+  <PlaceHolder v-else />
 </template>
 
 <script>
 import MainPage from "./components/MainPage.vue";
+import PlaceHolder from "./components/PlaceHolder/PlaceHolder.vue";
 
 export default {
   name: "App",
   components: {
     MainPage,
+    PlaceHolder,
+  },
+  data() {
+    return {
+      windowWidth: window.innerWidth,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.windowWidth = window.innerWidth;
+    },
   },
 };
 </script>
---
+++ src/components/PlaceHolder/PlaceHolder.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="placeholder-container">
+    <h2>Please open this site on desktop. Adaptive design is not ready</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlaceHolder",
+};
+</script>
+
+<style scoped>
+.placeholder-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  padding: 0 5vw 0;
+}
+
+h2 {
+  text-align: center;
+}
+</style>
